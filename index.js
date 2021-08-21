e24c653470ccde9a764d0b963b021d1be5476ea5
ZhenyeLee <73271404+ZhenyeLee@users.noreply.github.com>
2021-08-21 14:29:44 -0500

    Authentication part

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 require("./models/User");
 require("./services/passport");
 
@@ -12,8 +14,18 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //in ms
+    keys: [keys.cookieKey],
+  })
+);
+
+// telling Passport to use cookies to manage  authentication.
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
-//mongodb+srv://youran:<password>@feedbackcollection.zqvk9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,17 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
+passport.serializeUser((user, done) => {
+  done(
+    null, //error object,
+    user.id //_id from mongodb
+  );
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -15,8 +26,17 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
-      //take this model instance and save it into database
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //we have a record with the given profile id
+          done(null, existingUser);
+        } else {
+          //take this model instance and save it into database
+          new User({ googleId: profile.id }).save().then((user) => {
+            done(null, user);
+          });
+        }
+      });
     }
   )
 );
