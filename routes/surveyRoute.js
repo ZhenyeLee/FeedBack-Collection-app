01bc7b19ba9643516268d212ed37deff8f65b4bb
ZhenyeLee <73271404+ZhenyeLee@users.noreply.github.com>
2021-08-26 23:24:09 -0500

    Update surveyRoute.js

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -8,8 +8,8 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.get("/api/surveys/thanks", (req, res) => {
-    res.sendFile("thanksTemplate.html", { root: __dirname });
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+    res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
